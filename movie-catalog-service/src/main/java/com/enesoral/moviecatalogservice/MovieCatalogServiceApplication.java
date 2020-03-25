9da456b4d729c60befe541ad7e442119fa42fa09
M. Enes Oral <oralenes0@gmail.com>
2020-03-26 00:16:51 +0300

    Convert restTemplate to webClientBuilder

--- movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
@@ -11,6 +12,11 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
 	}
 
 	public static void main(String[] args) {
--- movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,11 +3,11 @@ package com.enesoral.moviecatalogservice.resources;
 import com.enesoral.moviecatalogservice.models.CatalogItem;
 import com.enesoral.moviecatalogservice.models.Movie;
 import com.enesoral.moviecatalogservice.models.Rating;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,9 +18,11 @@ import java.util.stream.Collectors;
 public class MovieCatalogResource {
 
     private final RestTemplate restTemplate;
+    private final WebClient.Builder webClientBuilder;
 
-    public MovieCatalogResource(RestTemplate restTemplate) {
+    public MovieCatalogResource(RestTemplate restTemplate, WebClient.Builder webClientBuilder) {
         this.restTemplate = restTemplate;
+        this.webClientBuilder = webClientBuilder;
     }
 
     @RequestMapping("/{userId}")
@@ -32,7 +34,13 @@ public class MovieCatalogResource {
         );
 
         return ratings.stream().map(rating -> {
-                Movie movie = restTemplate.getForObject("localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                //Movie movie = restTemplate.getForObject("localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                Movie movie = webClientBuilder.build()
+                        .get()
+                        .uri("localhost:8082/movies/" + rating.getMovieId())
+                        .retrieve()
+                        .bodyToMono(Movie.class)
+                        .block();
                 return new CatalogItem(movie.getName(), "Test description", rating.getRating());
         }).collect(Collectors.toList());
     }
