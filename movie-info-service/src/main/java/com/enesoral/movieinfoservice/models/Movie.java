04f631ebb23c65d5a0d90743dce5ca79dd633359
M. Enes Oral <oralenes0@gmail.com>
2020-03-25 22:01:38 +0300

    Add movie info api

---
+++ movie-info-service/src/main/java/com/enesoral/movieinfoservice/models/Movie.java
@@ -0,0 +1,28 @@
+package com.enesoral.movieinfoservice.models;
+
+public class Movie {
+
+    private String movieId;
+    private String name;
+
+    public Movie(String movieId, String name) {
+        this.movieId = movieId;
+        this.name = name;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
