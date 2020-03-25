7917767b3b2e5d460eb3ff1f986ce7669d77c0e2
M. Enes Oral <oralenes0@gmail.com>
2020-03-25 22:38:14 +0300

    Hard coded ratings in catalog service

--- movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,12 +1,14 @@
 package com.enesoral.moviecatalogservice.resources;
 
 import com.enesoral.moviecatalogservice.models.CatalogItem;
+import com.enesoral.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,8 +17,13 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        return Collections.singletonList(
-                new CatalogItem("Interstellar", "Test description", 5)
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
         );
+
+        return ratings.stream().map(rating ->
+                new CatalogItem("Interstellar", "Test description", 5))
+                .collect(Collectors.toList());
     }
 }
