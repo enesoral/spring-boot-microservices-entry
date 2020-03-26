36885b2bb2715eeaa427c97a3a234b28637c7c4d
M. Enes Oral <oralenes0@gmail.com>
2020-03-26 21:39:01 +0300

    Convert services to eureka clients

--- movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,11 +2,13 @@ package com.enesoral.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
 	@Bean
