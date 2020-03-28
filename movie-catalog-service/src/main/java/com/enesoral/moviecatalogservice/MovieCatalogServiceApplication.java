a0511126c27cbce2e07cfe8ff74b7c7d98218c53
M. Enes Oral <oralenes0@gmail.com>
2020-03-28 16:35:50 +0300

    Set timeout

--- movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -15,7 +16,9 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(3000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 
 	public static void main(String[] args) {
