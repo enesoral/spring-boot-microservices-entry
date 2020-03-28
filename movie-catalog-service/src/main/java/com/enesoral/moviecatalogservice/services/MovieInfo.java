efb8de285a9f824de488f592a25cd47387bb4627
M. Enes Oral <oralenes0@gmail.com>
2020-03-28 23:21:11 +0300

    Apply bulkhead pattern to movieInfo service

--- movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/services/MovieInfo.java
+++ movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/services/MovieInfo.java
@@ -17,12 +17,12 @@ public class MovieInfo {
         this.restTemplate = restTemplate;
     }
 
-    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem",
-            commandProperties = {
-                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
-                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
-                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
-                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000"),
+    @HystrixCommand(
+            fallbackMethod = "getFallbackCatalogItem",
+            threadPoolKey = "movieInfoPool",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "20"),
+                    @HystrixProperty(name = "maxQueueSize", value = "10")
             })
     public CatalogItem getCatalogItem(Rating rating) {
         Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
