82c83e3ca49015f791fc22b6f12c1f0fbc51b4d3
M. Enes Oral <oralenes0@gmail.com>
2020-03-28 20:30:41 +0300

    Implement hystrix and fallback method

--- movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/enesoral/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,11 +3,13 @@ package com.enesoral.moviecatalogservice.resources;
 import com.enesoral.moviecatalogservice.models.CatalogItem;
 import com.enesoral.moviecatalogservice.models.Movie;
 import com.enesoral.moviecatalogservice.models.UserRating;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,16 +24,21 @@ public class MovieCatalogResource {
     }
 
     @RequestMapping("/{userId}")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
         UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratings/user/" + userId, UserRating.class);
 
-        return userRating.getUserRatings().stream().map(rating -> {
+        return userRating.getUserRatings().stream()
+                .map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getTitle(), movie.getOverview(), rating.getRating());
+                })
+                .collect(Collectors.toList());
+    }
 
-                Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-                return new CatalogItem(movie.getTitle(), movie.getOverview(), rating.getRating());
-
-        }).collect(Collectors.toList());
+    private List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(new CatalogItem("No movie", "No description", 0));
     }
 }
 
